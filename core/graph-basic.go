188d3776ce7345da762c689688d29449cf628d5d
lempiy <lempiyada@gmail.com>
2019-07-10 00:01:47 +0300

    Finish traverse func

--- core/graph-basic.go
+++ core/graph-basic.go
@@ -6,7 +6,7 @@ import (
 
 func isMultiple(m map[string][]string, id string) bool {
 	list, ok := m[id]
-	return ok && len(list) > 1;
+	return ok && len(list) > 1
 }
 
 type Set map[string]struct{}
@@ -20,28 +20,37 @@ func (s *Set) Has(key string) bool {
 	return ok
 }
 
+func (s *Set) Copy() *Set {
+	copy := make(Set)
+	m := *s
+	for v := range m {
+		copy[v] = struct{}{}
+	}
+	return &copy
+}
+
 type relationMap map[string][]string
 type nodesMap map[string]NodeInput
 
 type GraphBasic struct {
-	list []NodeInput
-	nodesMap nodesMap
-	incomesByNodeIdMap relationMap
+	list                []NodeInput
+	nodesMap            nodesMap
+	incomesByNodeIdMap  relationMap
 	outcomesByNodeIdMap relationMap
-	loopsByNodeIdMap relationMap
+	loopsByNodeIdMap    relationMap
 }
 
 func NewGraphBasic(list []NodeInput) (*GraphBasic, error) {
 	g := &GraphBasic{
-		list: list,
-		nodesMap: make(nodesMap),
-		incomesByNodeIdMap: make(relationMap),
+		list:                list,
+		nodesMap:            make(nodesMap),
+		incomesByNodeIdMap:  make(relationMap),
 		outcomesByNodeIdMap: make(relationMap),
-		loopsByNodeIdMap: make(relationMap),
+		loopsByNodeIdMap:    make(relationMap),
 	}
 	for _, node := range g.list {
 		if _, ok := g.nodesMap[node.Id]; ok {
-			return nil, fmt.Errorf("Duplicate node id `%s`", node.Id)
+			return nil, fmt.Errorf("duplicate node id `%s`", node.Id)
 		}
 		g.nodesMap[node.Id] = node
 	}
@@ -53,8 +62,8 @@ func (g *GraphBasic) detectIncomesAndOutcomes() (err error) {
 		if totalSet.Has(node.Id) {
 			continue
 		}
-		branchSet :=  make(Set)
-		_ , err = g.traverseVertically(node, &branchSet, &totalSet)
+		branchSet := make(Set)
+		_, err = g.traverseVertically(node, &branchSet, &totalSet)
 		if err != nil {
 			return
 		}
@@ -62,8 +71,9 @@ func (g *GraphBasic) detectIncomesAndOutcomes() (err error) {
 }
 
 func (g *GraphBasic) traverseVertically(node NodeInput, branchSet *Set, totalSet *Set) (*Set, error) {
+	var err error
 	if branchSet.Has(node.Id) {
-		return nil, fmt.Errorf("Duplicate incomes for node id `%s`", node.Id)
+		return nil, fmt.Errorf("duplicate incomes for node id `%s`", node.Id)
 	}
 	branchSet.Add(node.Id)
 	totalSet.Add(node.Id)
@@ -74,10 +84,17 @@ func (g *GraphBasic) traverseVertically(node NodeInput, branchSet *Set, totalSet
 		}
 		// detect loops
 		if branchSet.Has(outcomeId) {
-			addUniqueRelation(g.loopsByNodeIdMap, node.Id, outcomeId)
+			addUniqueRelation(&g.loopsByNodeIdMap, node.Id, outcomeId)
+			continue
 		}
-		// @TODO check if addUniqueRelation works
+		addUniqueRelation(&g.incomesByNodeIdMap, outcomeId, node.Id)
+		addUniqueRelation(&g.outcomesByNodeIdMap, node.Id, outcomeId)
+		totalSet, err = g.traverseVertically(g.nodesMap[outcomeId], branchSet.Copy(), totalSet)
+		if err != nil {
+			return nil, err
+		}
 	}
+	return totalSet, nil
 }
 
 func (g *GraphBasic) IsLoopEdge(nodeId string, outcomeId string) bool {
@@ -93,10 +110,10 @@ func (g *GraphBasic) IsLoopEdge(nodeId string, outcomeId string) bool {
 	return false
 }
 
-func addUniqueRelation(rm relationMap, key, value string) {
-	relations, ok := rm[key]
+func addUniqueRelation(rm *relationMap, key, value string) {
+	relations, ok := (*rm)[key]
 	if !ok {
-		rm[key] = []string{value}
+		(*rm)[key] = []string{value}
 	}
 	isUnique := true
 	for _, v := range relations {
@@ -106,6 +123,6 @@ func addUniqueRelation(rm relationMap, key, value string) {
 		}
 	}
 	if isUnique {
-		rm[key] = append(relations, value)
+		(*rm)[key] = append(relations, value)
 	}
 }
